09cf8868f3121dbf150961e4dcfa3243c426cc8a
Sean Liao <seankhliao@gmail.com>
2020-05-14 23:48:50 +0200

    switch to gtm

--- webstyle.go
+++ webstyle.go
@@ -516,8 +516,34 @@ cn();
 <html lang="en">
   <head>
     {{ template "HeadGohtml" . }}
+
+    <!-- Google Tag Manager -->
+    <script>
+      (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", "GTM-TLVN7D6");
+    </script>
+    <!-- End Google Tag Manager -->
   </head>
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-TLVN7D6"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden;"
+      ></iframe
+    ></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <header>
       {{ template "LogoHtml" }}
       {{ .Header }}
@@ -542,21 +568,6 @@ cn();
         navigator.sendBeacon(`+"`"+`{{ .URLLogger }}?trigger=beacon&src={{ .URLCanonical }}&dst=${dst}&dur=${ts1 - ts0}ms`+"`"+`);
       });
     </script>
-
-    {{ if .GoogleAnalytics }}
-    <script>
-      "use strict";
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-      gtag("config", "{{ .GoogleAnalytics }}");
-    </script>
-    <script>
-      {{ template "GtagJs" }}
-    </script>
-    {{ end }}
   </body>
 </html>
 {{ end }}`
